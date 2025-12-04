ad7a18f37e9f7c659938b579c0c4e72b84b8ce50
marsel224 <marselkolcova224@gmail.com>
2025-12-04 10:06:57 +0300

    прописала глобальные компоненты

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button class="btn">
+        <slot />
+    </button>
+    </template>
+
+    <style scoped>
+    .btn {
+        padding: 12px 22px;
+        border-radius: var(--radius);
+        border: none;
+        background: var(--primary);
+        color: white;
+        tont-weight: bold;
+        cursor: pointer;
+    }
+</style>
---
+++ src/components/BaseContainer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+     <slot />
+    </div>
+</template>
+
+<style scoped>
+.container {
+    max-width: var(--container);
+    margin: 0 aut;
+    padding: 0 20px;
+}
+</style>
