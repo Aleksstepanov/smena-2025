ef9257b2507ebdaae6f3356089f76b0101fec61a
Ax1ck <dima_vasilev_04@bk.ru>
2025-12-06 11:23:34 +0300

    Адаптив и сетка

--- Sec2026/Sec2026/src/components/BaseContainer.vue
+++ Sec2026/Sec2026/src/components/BaseContainer.vue
@@ -5,10 +5,36 @@
 </template>
 
 <style scoped>
+/* Базовые настройки контейнера */
+
 .container {
+  max-width: 1280px;   /* Основная ширина контента */
+  margin: 0 auto;      /* Центровка на десктопе */
+  padding: 0 20px;     /* Мобильные отступы по бокам */
+
   width: 100%;
-  max-width: var(--container-max);
-  padding: 0 var(--container-padding);
-  margin: 0 auto;
+}
+
+/* ПЛАНШЕТЫ */
+@media (min-width: 768px) {
+  .container {
+    padding: 0 32px;
+    max-width: 1440px; /* на планшете центрирование + немного шире */
+  }
+}
+
+/* НОУТБУКИ И ДЕСКТОП */
+@media (min-width: 1024px) {
+  .container {
+    padding: 0 48px;
+    max-width: 1440px;
+  }
+}
+
+/* ШИРОКИЕ ЭКРАНЫ — 4K */
+@media (min-width: 1600px) {
+  .container {
+    max-width: 1600px;
+  }
 }
 </style>
