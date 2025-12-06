d4eaec3817f0e7993f4bf8f30daf27459b114086
Ax1ck <dima_vasilev_04@bk.ru>
2025-12-06 10:33:02 +0300

    Структура проекта и верстка Hero

---
+++ Sec2026/Sec2026/src/components/BaseContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100%;
+  max-width: var(--container-max);
+  padding: 0 var(--container-padding);
+  margin: 0 auto;
+}
+</style>
