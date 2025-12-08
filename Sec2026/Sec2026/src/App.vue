dc51d683e8b3f4822bf5b799e3d39132bcaa8cd4
programm <Hoboe44.44@yandex.ru>
2025-12-08 09:30:39 +0300

    Добавила About и Head

--- Sec2026/Sec2026/src/App.vue
+++ Sec2026/Sec2026/src/App.vue
@@ -1,11 +1,17 @@
 <template>
+  <Header />
+  <About />
   <HeroSection />
   <ProgramSection />
 </template>
 
 <script setup>
 import HeroSection from './sections/HeroSection.vue'
+import About from './sections/About.vue'
 import ProgramSection from './sections/ProgramSection.vue'
+import Header from './sections/Header.vue'
+//import Header from './sections/Header.vue'
+//import About from './sections/About.vue'
 //import Program from './sections/Program.vue'
 //import Speakers from './sections/Speakers.vue'
 //import Sponsors from './sections/Sponsors.vue'
