46d7c511c441381fa5b39daccd28786e07c6d5c4
Ax1ck <dima_vasilev_04@bk.ru>
2025-12-10 10:44:33 +0300

    APP

--- Sec2026/Sec2026/src/App.vue
+++ Sec2026/Sec2026/src/App.vue
@@ -2,10 +2,12 @@
   <Header />
   <HeroSection />
   <About />
-  <FAQ />
   <ProgramSection />
   <SponsorsSection />
   <FooterSection />
+  <SpeakersSection />
+  <Map />
+  <FAQ />
 </template>
 
 <script setup>
@@ -13,7 +15,15 @@ import HeroSection from './sections/HeroSection.vue'
 import About from './sections/About.vue'
 import ProgramSection from './sections/ProgramSection.vue'
 import Header from './sections/Header.vue'
+import SpeakersSection from './sections/SpeakersSection.vue'
 import FAQ from './sections/FAQ.vue'
-import SponsorsSection from './sections/SponsorsSection.vue'
-import FooterSection from './sections/FooterSection.vue'
+import Map from './sections/Map.vue'
+//import FAQ from './sections/FAQ.vue'
+//import Header from './sections/Header.vue'
+//import About from './sections/About.vue'
+//import Program from './sections/Program.vue'
+//import Speakers from './sections/Speakers.vue'
+//import Sponsors from './sections/Sponsors.vue'
+//import FAQ from './sections/FAQ.vue'
+//import Footer from './sections/Footer.vue'
 </script>
